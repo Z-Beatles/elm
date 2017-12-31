ce35a7e95aa338c225e35a7dd5fc79902f102002
Wayne Chu <waynechu@waynechu.cn>
2017-12-31 17:52:42 +0800

    完成header组件

---
+++ src/components/seller/seller.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>I am sellers!</div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Goods from '@/components/goods/goods';
+import Ratings from '@/components/ratings/ratings';
+import Seller from '@/components/seller/seller';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  linkActiveClass: 'active',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/goods',
+      name: 'goods',
+      component: Goods
+    },
+    {
+      path: '/ratings',
+      name: 'ratings',
+      component: Ratings
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: Seller
     }
   ]
-})
+});
