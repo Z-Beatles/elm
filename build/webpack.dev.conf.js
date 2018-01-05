6a4bec99bab972e5bd2b000ff390b9f30dfa38f0
Wayne Chu <waynechu@waynechu.cn>
2018-01-05 13:47:24 +0800

    修改disableHostCheck，解决远程访问出现invalid host header

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -46,6 +46,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     watchOptions: {
       poll: config.dev.poll,
     },
+    disableHostCheck: true,
     //然后找到devServer,在里面添加
     before(app) {
       app.get('/api/seller', (req, res) => {
